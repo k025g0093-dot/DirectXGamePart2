838ac1bbfbed248807f2d5179f0eb69a006f898f
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-06-26 12:45:02 +0900

    確認課題終了

--- Enemy.cpp
+++ Enemy.cpp
@@ -3,11 +3,7 @@
 
 using namespace KamataEngine;
 
-void Enemy::Initialize(
-	KamataEngine::Model* model, 
-	KamataEngine::Camera* camera,
-	const KamataEngine::Vector3& position
-) {
+void Enemy::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position) {
 
 	assert(model); // セッターにモデルを入れる
 	model_ = model;
@@ -17,19 +13,42 @@ void Enemy::Initialize(
 	worldTransform_.rotation_ = {0.0f, 180.0f, 0.0f};
 	velocity_ = {0, 0, -0.01f};
 	camera_ = camera;
-
 }
 
 void Enemy::Update() {
 
+	switch (phase_) {
+	case Phase::Approach:
+
+		UpdateApproach();
+
+		break;
+	case Phase::Leave:
+
+		UpdateLeave();
+
+		break;
+	default:
+		break;
+	}
+
+	UpdateWorldTransform(worldTransform_);
+}
+
+void Enemy::Draw() { model_->Draw(worldTransform_, *camera_); }
+
+void Enemy::UpdateApproach() {
 	worldTransform_.translation_.x += velocity_.x;
 	worldTransform_.translation_.y += velocity_.y;
 	worldTransform_.translation_.z += velocity_.z;
-	
-	UpdateWorldTransform(worldTransform_);
 
+	if (worldTransform_.translation_.z <= 0.0f) {
+		velocity_ = {0.01f, 0.01f, 0};
+		phase_ = Phase::Leave;
+	}
 }
 
-void Enemy::Draw() {
-	model_->Draw(worldTransform_, *camera_);
+void Enemy::UpdateLeave() {
+	worldTransform_.translation_.x += velocity_.x;
+	worldTransform_.translation_.y += velocity_.y;
 }
--- Enemy.h
+++ Enemy.h
@@ -2,20 +2,19 @@
 #include "KamataEngine.h"
 #include "MyMath.h"
 
+enum class Phase {
+	Approach,//接近
+	Leave,//離脱
+};
+
 class Enemy {
 
 public:
-
-	void Initialize(
-		KamataEngine::Model* model,
-		KamataEngine::Camera* camera, 
-		const KamataEngine::Vector3& position);
+	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position);
 	void Update();
 	void Draw();
 
-
 private:
-
 	// ワールドトランスフォーム
 	KamataEngine::WorldTransform worldTransform_;
 	// 3Dモデルで必要なモデルの呼び出し
@@ -24,5 +23,12 @@ private:
 	KamataEngine::Camera* camera_ = nullptr;
 	// 敵の移動速度
 	KamataEngine::Vector3 velocity_ = {};
+
+private:
+
+	Phase phase_ =Phase::Approach;
+
+	void UpdateApproach();
+	void UpdateLeave();
 
 };
